95104ef41d0662417edfd1f81674a9b7ceaafa61
Jorge Carrera <JCarrera994@gmail.com>
2022-07-30 17:47:26 -0400

    adds alt tag to image

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ import logo from "../images/money-map.jpg";
 export default function Home() {
   return (
     <div className="article">
-      <img src={logo} className="homeImg" />
+      <img src={logo} alt='map-home-page' className="homeImg" />
       <section className="h1Section">
       <h1 className="homeH1 font-link">Conquer your wallet, then the World...</h1>
       </section>
